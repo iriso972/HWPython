1c6fbf9655dc8b2c8f733d1adc623015b049dfc5
Iriso972 <skyiriso972@gmail.com>
2025-07-16 14:04:21 +0300

    Обновление

--- 05_lesson/lesson05_task04.py
+++ 05_lesson/lesson05_task04.py
@@ -3,7 +3,6 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.firefox import GeckoDriverManager
 
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
@@ -16,7 +15,7 @@ password_input = driver.find_element(By.ID,"password")
 password_input.send_keys("SuperSecretPassword!")
 sleep(5)
 
-login_button = driver.find_elements(By.ID, "button")
+login_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
 login_button.click()
 
 flash_message = driver.find_element(By.CSS_SELECTOR, "div.flash.success")
