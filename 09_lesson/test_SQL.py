d2c74071ad62999ff7d3387b92cd39a03f23e368
Iriso972 <skyiriso972@gmail.com>
2025-08-18 22:51:51 +0300

    lesson_08

---
+++ 09_lesson/test_SQL.py
@@ -0,0 +1,74 @@
+from sqlalchemy import create_engine, inspect, text, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Создаем подключение к базе данных
+db_connection_string = "postgresql://postgres:1@localhost:5432/TEST_DB"
+db = create_engine(db_connection_string)
+Session = sessionmaker(bind=db)
+session = Session()
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key=True)
+    user_email = Column(String, primary_key=True)
+    subject_id = Column(Integer, primary_key=True)
+
+
+# Добавление нового пользователя
+def test_add_user():
+    session = Session()
+    users = User(user_id=18, user_email="sashaivanov@mail.ru", subject_id=1)
+    session.add(users)
+    session.commit()
+
+    saved = session.query(User).filter_by(user_id=18).first()
+    assert saved.user_email =="sashaivanov@mail.ru"
+
+    session.delete(saved)
+    session.commit()
+    session.close()
+
+
+# Изменение user_email пользователя
+def test_query_user():
+    session = Session()
+    users = User(user_id=18, user_email= "sashaivanov@mail.ru", subject_id=1)
+    session.add(users)
+    session.commit()
+
+    saved = session.query(User).filter_by(user_id=18).first()
+    assert saved.user_email == "sashaivanov@mail.ru"
+
+    users = User(user_id=18, user_email= "mashaivanova@mail.ru", subject_id=1)
+    session.query()
+    session.commit()
+
+    saved = session.query(User).filter_by(user_id=18).first()
+    assert saved.user_email == "mashaivanova@mail.ru"
+
+    # Удаление тестовых данных
+    session.delete(saved)
+    session.commit()
+    session.close()
+
+# Удаление пользователя
+
+
+def test_delete_user():
+    session = Session()
+    users = User(user_id=18, user_email="sashaivanov@mail.ru", subject_id=1)
+    session.add(users)
+    session.commit()
+
+    saved = session.query(User).filter_by(user_id=18).first()
+    assert saved.user_email == "sashaivanov@mail.ru"
+
+    # Удаление пользователя
+    session.delete(saved)
+    session.commit()
+
+    # Закрытие сессии
+    session.close()
